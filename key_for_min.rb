e9bfa4b86a9d97421bc9708a845d369f65bc76f3
Nathan Burns <nathanandreburns@gmail.com>
2020-08-06 03:45:20 +0000

    Done.

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,5 +2,13 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
-
+  array_ordered_by_age = []
+  array_ordered_by_age = name_hash.collect {|name, age|
+    age}.sort
+  name_hash.each do |name, age|
+    if age == array_ordered_by_age[0]
+      return name
+    end
+  end
+  nil 
 end
